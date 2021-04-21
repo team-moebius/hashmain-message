dfc85c46cc5c73d755162d1d1b0fd0d2a8897e62
ghost9087 <ghost9087@gmail.com>
2021-04-21 19:47:29 +0900

    [HMM-0] Dispose previously started disposable for kafka receiver and start new one on failure

--- consumer/src/main/java/com/moebius/message/consumer/kafka/KafkaConsumer.java
+++ consumer/src/main/java/com/moebius/message/consumer/kafka/KafkaConsumer.java
@@ -2,6 +2,7 @@ package com.moebius.message.consumer.kafka;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import reactor.core.Disposable;
 import reactor.core.scheduler.Schedulers;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
@@ -12,19 +13,19 @@ import java.util.Map;
 
 @Slf4j
 public abstract class KafkaConsumer<K, V> {
-    private final KafkaReceiver<K, V> receiver;
+    private final ReceiverOptions<K, V> receiverOptions;
+    private KafkaReceiver<K, V> receiver;
+    private Disposable receiverDisposable;
 
     public KafkaConsumer(ReceiverOptions<?, ?> baseReceiverOptions) {
         Map<String, Object> consumerProperties = baseReceiverOptions.consumerProperties();
         consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, getKeyDeserializerClass());
         consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, getValueDeserializerClass());
 
-        ReceiverOptions<K, V> receiverOptions = ReceiverOptions.create(consumerProperties);
+        receiverOptions = ReceiverOptions.create(consumerProperties);
         receiverOptions.subscription(Collections.singleton(getTopic()))
                 .addAssignListener(partitions -> log.debug("[Kafka] onPartitionsAssigned {}", partitions))
                 .addRevokeListener(partitions -> log.debug("[Kafka] onPartitionsRevoked {}", partitions));
-
-        receiver = KafkaReceiver.create(receiverOptions);
     }
 
     public abstract String getTopic();
@@ -37,7 +38,11 @@ public abstract class KafkaConsumer<K, V> {
 
     public void consumeMessages() {
         log.info("[Kafka] Start to read messages. [{}]", getTopic());
-        receiver.receive()
+        if (receiverDisposable != null && !receiverDisposable.isDisposed()) {
+            receiverDisposable.dispose();
+        }
+        receiver = KafkaReceiver.create(receiverOptions);
+        receiverDisposable = receiver.receive()
                 .publishOn(Schedulers.elastic())
                 .doOnTerminate(this::consumeMessages)
                 .subscribe(this::processRecord);
